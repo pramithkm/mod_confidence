7312f4cd816f18d69cc207c388193762d04c3c23
Pramith Dayananda <pramithd@catalyst.net.nz>
2016-10-04 13:53:28 +1300

    mod/confidece: remove extra mod folder inside confidence
    WR#262654

--- mod/reengagement/db/install.php
+++
@@ -1,7 +0,0 @@
-<?php
-function xmldb_reengagement_install() {
-    global $DB;
-    $settings = array('s__enablecompletion' => 1, 's__enableavailability' => 1);
-    admin_write_settings($settings);
-}
-?>
--- mod/workshop/db/install.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function xmldb_workshop_install() {
-    global $DB;
-
-    $DB->set_field('modules', 'visible', 0, array('name'=>'workshop'));
-}
-
